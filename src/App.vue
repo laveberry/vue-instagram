92a2d2ef4513e70ec0a464396809ed6c6cf3166c
laveberry <laveberry7@gmail.com>
2021-08-23 10:39:55 +0900

    post 작성 개발중

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- {{postData}} -->
+    <!-- prop 전달 -->
+    <Post v-bind:postData="postData"/>
+  </div>
+</template>
+
+<script>
+import data from './assets/data.js';
+import Post from './components/Post.vue'
+
+export default {
+  data(){
+    return {
+      postData : data
+    }
+  },
+  components : {
+    Post
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <!-- {{postData}} -->
+      <h2>{{postData[0].title}}</h2>
+      <p><img v-bind:src="postData[0].img" alt=""></p>
+      <p>글쓴이 : {{postData[0].author}}</p>
+      <p>{{postData[0].caption}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'post',
+    //넘어온 prop 받기위한 작업
+    props : {
+        'postData' : Object
+    }
+}
+</script>
+
+<style>
+
+</style>
