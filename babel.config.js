491dae23e8bc7240a3189c70295d991114889a9b
laveberry <laveberry7@gmail.com>
2021-09-06 15:55:35 +0900

    spring boot 연결 진행중

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,18 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+  // presets: [
+  //   '@vue/cli-plugin-babel/preset'
+  // ]
+  
+  //스프링부트 연결
+  outputDir : '../src.main/resources/static',
+  indexpath : "../static/index.html",
+  devServer : {
+    port : 9091,
+    proxy : "http://localhost:9191", //devServer : spring boot의 내장 was주소
+  },
+  chainWebpack : config => {
+    const svgRule = confing.module.rule("svg");
+    svgRule.uses.clear();
+    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+  }
+};
